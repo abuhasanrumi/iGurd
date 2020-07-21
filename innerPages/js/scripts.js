2e6236dea0a1321fc136c0e45d1fe4b63b0bf91c
Abu Hasan Rumi <message.rumi@gmail.com>
2020-07-21 06:47:30 +0600

    completed full dashboard

--- innerPages/js/scripts.js
+++ innerPages/js/scripts.js
@@ -1,7 +1,8 @@
 $(document).on('ready', function () {
     // initialization of unfold
     var unfold = new HSUnfold('.js-hs-unfold-invoker').init();
-    
+    document.getElementById('chat-box').scrollTop = 9999999;
+
   });
   $('.dz-dropzone').each(function () {
       // initialization of dropzone file attach module
