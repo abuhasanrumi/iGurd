e7899821b002f86430617ba344b5f4f0fd879411
Abu Hasan Rumi <message.rumi@gmail.com>
2020-07-23 14:09:49 +0600

    homepage v1.3

--- innerPages/js/scripts.js
+++ innerPages/js/scripts.js
@@ -1,10 +1,58 @@
-$(document).on('ready', function () {
-    // initialization of unfold
-    var unfold = new HSUnfold('.js-hs-unfold-invoker').init();
-    document.getElementById('chat-box').scrollTop = 9999999;
+function openNav() {
+  document.getElementById("mySidenav").style.width = "235px";
+  document.getElementById("mySidenav").style.paddingRight = "1.5rem";
+  document.getElementById("mySidenav").style.paddingLeft = "1.5rem";
+}
 
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.paddingRight = "0rem";
+  document.getElementById("mySidenav").style.paddingLeft = "0rem";
+}
+
+
+
+$(document).ready(function(){
+  $('.portfolio-wrap').owlCarousel({
+    items:3,
+    loop:true,
+    margin:50,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    responsive:{
+      0:{
+          items:1,
+      },
+      600:{
+          items:1,
+      },
+      1000:{
+          items:3,
+      }
+      }
   });
-  $('.dz-dropzone').each(function () {
-      // initialization of dropzone file attach module
-      var dropzone = $.HSCore.components.HSDropzone.init('#' + $(this).attr('id'));
-    });
+
+  $('.partnerCarousel').owlCarousel({
+    items:5,
+    loop:true,
+    center:true,
+    margin:20,
+    dots: false,
+    nav: 2,
+    autoplay:false,
+    navText : ['<i class="fas fa-arrow-alt-circle-left"></i>','<i class="fas fa-arrow-alt-circle-right"></i>'],
+    responsive:{
+      0:{
+          items:1,
+      },
+      600:{
+          items:2,
+      },
+      1000:{
+          items:5,
+      }
+      }
+  });
+
+});
